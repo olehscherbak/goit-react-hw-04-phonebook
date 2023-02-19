dccd6c480ee084992987c4f4e4fa6a89fc169ec0
Oleh.Scherbak <oleh.scherbak@ukr.net>
2023-02-19 18:11:29 +0200

    Update useLocalStorage.js

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -4,6 +4,9 @@ export default function useLocalStorage(key, defaultValue) {
   const [state, setState] = useState(() => {
     return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
   });
-  useEffect(() => window.localStorage.setItem(key, JSON.stringify(state)));
+  useEffect(
+    () => window.localStorage.setItem(key, JSON.stringify(state)),
+    [key, state]
+  );
   return [state, setState];
 }
